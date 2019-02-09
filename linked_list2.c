7d81118d7837cc894ec039461d118632145a7493
Neesara <neethums333@gmail.com>
2019-02-10 01:43:31 +0530

    some more

---
+++ linked_list2.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void main()
+{
+
+struct node{
+
+ int val;
+ struct node *next;
+
+}*head=NULL,*traverse,*tmp;
+
+int num,i;
+
+printf("Enter the no. of numbers :");
+scanf("%d",&num);
+
+printf("Enter the numbers :");
+for (i=0;i<num;i++)
+{
+
+tmp=( struct node*) malloc(sizeof(struct node));
+scanf("%d",&tmp->val);
+tmp->next=NULL;
+
+if (head == NULL)
+{
+  traverse=tmp;
+  head=tmp;
+}
+else
+{
+traverse->next=tmp;
+traverse=traverse->next;
+}
+}
+
+traverse=head;
+while(traverse != NULL)
+{
+
+printf("%d\t",traverse->val);
+traverse=traverse->next;
+}
+
+
+}
---
+++ swap_hex.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+  int a,b,c,d;
+
+scanf("%x",&a);
+  b=0x00FF;
+  c=0xFF00;
+ 
+  b=a & b;
+  c=a & c;
+
+  b=b << 8;
+  c=a >> 8;
+  
+  a=b | c;
+
+printf("%x\n",a); 
+}
